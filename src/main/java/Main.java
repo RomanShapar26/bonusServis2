ec0951fd940090782ad34c7da05ae85a67ebf9be
Роман Шапарь <romanshaparution@gmail.com>
2022-02-11 11:32:29 +0300

    fixed #3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,13 @@ public class Main {
   public static void main(String[] args) {
     BonusService service = new BonusService();
 
-    long amount = 1000_60;
-    boolean registered = true;
+//    long amount = 1000_60;
+//    boolean registered = true;
+    long bonus = service.calculate(1000_60, true);
 
-    service.calculate(amount, registered);
+    // строка с багом service.calculate(amount, registered);
+    System.out.println (bonus);
+
   }
 }
 
